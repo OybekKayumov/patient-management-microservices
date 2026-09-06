ecfe5f8f54d5876d452d00fdf23b8ad2ef5ed52a
Oybek Kayumov <oybek.kayumov@gmail.com>
2026-09-06 06:00:27 +0500

    Add Analytic Service

---
+++ analytics-service/src/main/java/com/ok/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,11 @@
+package com.ok.analyticsservice.kafka;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+	public void consumeEvent(byte[] event) {
+
+	}
+}
